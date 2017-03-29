107c70d2d598f71aea10c8959f18543e18a5132a
Felix Kaaman <trundmatu@gmail.com>
2017-03-29 12:04:34 +0200

    Initial commit

---
+++ Lil game/Lil game/main.cpp
@@ -0,0 +1,226 @@
+#include <windows.h>
+
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include "dxerr.h"
+#include <DirectXMath.h>
+#include <cstdio>
+#include <iostream>
+
+using namespace DirectX;
+
+#pragma comment (lib, "d3d11.lib")
+#pragma comment (lib, "d3dcompiler.lib")
+
+#define WIDTH 1280
+#define HEIGHT 800
+
+IDXGISwapChain *gSwapChain;
+ID3D11Device *gDevice;
+ID3D11DeviceContext *gDeviceContext;
+ID3D11RenderTargetView *gBackbufferRTV;
+ID3D11DepthStencilView *gDepthStencil;
+
+void CreateDepthBuffer()
+{
+	ID3D11Texture2D* pDepthStencil = NULL;
+	D3D11_TEXTURE2D_DESC descDepth;
+	descDepth.Width = WIDTH;
+	descDepth.Height = HEIGHT;
+	descDepth.MipLevels = 1;
+	descDepth.ArraySize = 1;
+	descDepth.Format = DXGI_FORMAT_D32_FLOAT;
+	descDepth.SampleDesc.Count = 1;
+	descDepth.SampleDesc.Quality = 0;
+	descDepth.Usage = D3D11_USAGE_DEFAULT;
+	descDepth.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+	descDepth.CPUAccessFlags = 0;
+	descDepth.MiscFlags = 0;
+	DXCALL(gDevice->CreateTexture2D(&descDepth, NULL, &pDepthStencil));
+
+	D3D11_DEPTH_STENCIL_DESC dsDesc;
+	dsDesc.DepthEnable = true;
+	dsDesc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL;
+	dsDesc.DepthFunc = D3D11_COMPARISON_LESS;
+	dsDesc.StencilEnable = true;
+	dsDesc.StencilReadMask = 0xFF;
+	dsDesc.StencilWriteMask = 0xFF;
+	dsDesc.FrontFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
+	dsDesc.FrontFace.StencilDepthFailOp = D3D11_STENCIL_OP_INCR;
+	dsDesc.FrontFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;
+	dsDesc.FrontFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
+	dsDesc.BackFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
+	dsDesc.BackFace.StencilDepthFailOp = D3D11_STENCIL_OP_DECR;
+	dsDesc.BackFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;
+	dsDesc.BackFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
+
+	ID3D11DepthStencilState * pDSState;
+	DXCALL(gDevice->CreateDepthStencilState(&dsDesc, &pDSState));
+
+	D3D11_DEPTH_STENCIL_VIEW_DESC descDSV;
+	ZeroMemory(&descDSV, sizeof(descDSV));
+	descDSV.Format = DXGI_FORMAT_D32_FLOAT;
+	descDSV.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+	descDSV.Texture2D.MipSlice = 0;
+
+	DXCALL(gDevice->CreateDepthStencilView(pDepthStencil, &descDSV, &gDepthStencil));
+}
+
+HRESULT CreateDirect3DContext(HWND wndHandle)
+{
+	DXGI_SWAP_CHAIN_DESC scd;
+	ZeroMemory(&scd, sizeof(DXGI_SWAP_CHAIN_DESC));
+
+	scd.BufferCount = 1;                                    // one back buffer
+	scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;     // use 32-bit color
+	scd.BufferDesc.RefreshRate.Numerator = 60;
+	scd.BufferDesc.RefreshRate.Denominator = 1;
+	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;      // how swap chain is to be used
+	scd.OutputWindow = wndHandle;                           // the window to be used
+	scd.SampleDesc.Count = 1;                               // how many multisamples
+	scd.Windowed = true;									// windowed/full-screen mode
+
+	HRESULT hr = D3D11CreateDeviceAndSwapChain(NULL,
+		D3D_DRIVER_TYPE_HARDWARE,
+		NULL,
+		D3D11_CREATE_DEVICE_DEBUG,
+		NULL,
+		NULL,
+		D3D11_SDK_VERSION,
+		&scd,
+		&gSwapChain,
+		&gDevice,
+		NULL,
+		&gDeviceContext);
+
+	if (SUCCEEDED(hr))
+	{
+		ID3D11Texture2D* pBackBuffer = nullptr;
+		DXCALL(gSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer));
+
+		DXCALL(gDevice->CreateRenderTargetView(pBackBuffer, NULL, &gBackbufferRTV));
+		pBackBuffer->Release();
+
+		CreateDepthBuffer();
+
+		// set the render target as the back buffer
+		gDeviceContext->OMSetRenderTargets(1, &gBackbufferRTV, gDepthStencil);
+	}
+
+	return hr;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+
+	return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+HWND InitWindow(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex = { 0 };
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.hInstance = hInstance;
+	wcex.lpszClassName = L"DX11_3D_PROJECT";
+	if (!RegisterClassEx(&wcex))
+		return false;
+
+	RECT rc = { 0, 0, WIDTH, HEIGHT };
+	//AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+
+	HWND handle = CreateWindow(
+		L"DX11_3D_PROJECT",
+		L"DX11 3D Project",
+		WS_VISIBLE | WS_SYSMENU,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		rc.right - rc.left,
+		rc.bottom - rc.top,
+		nullptr,
+		nullptr,
+		hInstance,
+		nullptr);
+
+	return handle;
+}
+
+HWND wndHandle;
+
+long long time_ms() {
+	static LARGE_INTEGER s_frequency;
+	static BOOL s_use_qpc = QueryPerformanceFrequency(&s_frequency);
+	if (s_use_qpc) {
+		LARGE_INTEGER now;
+		QueryPerformanceCounter(&now);
+		return (1000LL * now.QuadPart) / s_frequency.QuadPart;
+	}
+	else {
+		return GetTickCount();
+	}
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+	MSG msg = { 0 };
+	wndHandle = InitWindow(hInstance);
+
+	if (wndHandle) {
+		CreateDirect3DContext(wndHandle);
+
+		//SetViewport(WIDTH, HEIGHT);
+		//CreateRenderTargets(WIDTH, HEIGHT);
+
+		//Initialize();
+
+		ShowWindow(wndHandle, nCmdShow);
+
+		long long start = time_ms();
+		long long prev = start;
+
+		bool quit = false;
+		while (!quit) {
+			long long newtime = time_ms();
+			long long elapsed = newtime - prev;
+
+			while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+				if (msg.message == WM_QUIT)
+					quit = true;
+
+				if (msg.message == WM_KEYUP) {
+					int wk = msg.wParam;
+
+					if (wk == VK_ESCAPE) quit = true;
+
+					//KeyUp(wk);
+				}
+
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+
+
+			//Update((elapsed) / 1000.f);
+			//Render();
+
+			gSwapChain->Present(0, 0);
+			prev = newtime;
+		}
+
+		gBackbufferRTV->Release();
+		gSwapChain->Release();
+		gDevice->Release();
+		gDeviceContext->Release();
+
+		DestroyWindow(wndHandle);
+	}
+
+	return (int)msg.wParam;
+}
