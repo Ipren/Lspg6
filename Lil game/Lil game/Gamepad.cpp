6ba320ec876561d7d4537fecbd21b09c868e4385
Felix Kaaman <trundmatu@gmail.com>
2017-03-30 14:31:38 +0200

    Gamepad class start

---
+++ Lil game/Lil game/Gamepad.cpp
@@ -0,0 +1,111 @@
+#include "Gamepad.h"
+
+#include <math.h>
+
+#include "Globals.h"
+
+// saxat från https://msdn.microsoft.com/en-us/library/windows/desktop/ee417001(v=vs.85).aspx
+static void normalize_thumbs(float x, float y, float deadzone, float *out_x, float *out_y)
+{
+	float magnitude = sqrt(x * x + y * y);
+
+	float normalized_x = x / magnitude;
+	float normalized_y = y / magnitude;
+	float normalized_magnitude = 0;
+
+	if (magnitude > deadzone)
+	{
+		if (magnitude > 32767) magnitude = 32767;
+
+		magnitude -= deadzone;
+
+		normalized_magnitude = magnitude / (32767 - deadzone);
+	}
+	else
+	{
+		magnitude = 0.0;
+		normalized_magnitude = 0.0;
+	}
+
+	*out_x = normalized_x * normalized_magnitude;
+	*out_y = normalized_y * normalized_magnitude;
+}
+
+Gamepad::Gamepad(unsigned int index) :
+	index(index), reconn_time(0.f), left_thumb_x(0.f), left_thumb_y(0.f),
+	right_thumb_x(0.f), right_thumb_y(0.f)
+{
+	connected = XInputGetState(index, nullptr) == ERROR_SUCCESS;
+}
+
+Gamepad::~Gamepad()
+{
+}
+
+float Gamepad::get_left_thumb_x()
+{
+	return left_thumb_x;
+}
+
+float Gamepad::get_left_thumb_y()
+{
+	return left_thumb_y;
+}
+
+float Gamepad::get_right_thumb_x()
+{
+	return right_thumb_x;
+}
+
+float Gamepad::get_right_thumb_y()
+{
+	return right_thumb_y;
+}
+
+void Gamepad::update(float dt)
+{
+	XINPUT_STATE state;
+	ZeroMemory(&state, sizeof(XINPUT_STATE));
+
+	if (!connected) {
+		reconn_time += dt;
+
+		// efter RECONN_TIME sekunder kolla om vi har reconnectat igen
+		if (reconn_time > RECONN_TIME) {
+			reconn_time -= RECONN_TIME;
+		
+			connected = XInputGetState(index, &state) == ERROR_SUCCESS;
+			if (!connected) return;
+		}
+		else {
+			return;
+		}
+	}
+
+	if (XInputGetState(index, &state) == ERROR_SUCCESS) {
+		normalize_thumbs(
+			state.Gamepad.sThumbLX,
+			state.Gamepad.sThumbLY,
+			XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE,
+			&left_thumb_x,
+			&left_thumb_y
+		);
+
+		normalize_thumbs(
+			state.Gamepad.sThumbRX,
+			state.Gamepad.sThumbRY,
+			XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE,
+			&right_thumb_x,
+			&right_thumb_y
+		);
+	}
+	else {
+		connected = false;
+
+		left_thumb_x = 0.f;
+		left_thumb_y = 0.f;
+
+		right_thumb_x = 0.f;
+		right_thumb_y = 0.f;
+	}
+}
---
+++ Lil game/Lil game/Gamepad.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <Windows.h>
+#include <Xinput.h>
+
+class Gamepad
+{
+public:
+	Gamepad(unsigned int index);
+	~Gamepad();
+
+	void update(float dt);
+
+	float get_left_thumb_x();
+	float get_left_thumb_y();
+
+	float get_right_thumb_x();
+	float get_right_thumb_y();
+
+private:
+	unsigned int index;
+
+	float left_thumb_x;
+	float left_thumb_y;
+
+	float right_thumb_x;
+	float right_thumb_y;
+
+	bool connected;
+	// håll reda på hur länge vi har varit disconnectad för att kunna kolla efter
+	// reconnect varje n sekunder
+	float reconn_time;
+};
+
