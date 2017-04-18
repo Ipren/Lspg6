a6726134c722129792172771c31e1f22e265aeda
dreas94 <dreas94@gmail.com>
2017-04-18 17:19:57 +0200

    Created the beginning of the Upgrades.h

---
+++ Pushlock/Upgrades.h
@@ -0,0 +1,23 @@
+#pragma once
+
+struct Upgrades {
+	int arcanechoice1 = 0;
+	int arcanechoice2 = 0;
+	int arcanechoice3 = 0;
+
+	int firechoice1 = 0;
+	int firechoice2 = 0;
+	int firechoice3 = 0;
+
+	int windchoice1 = 0;
+	int windchoice2 = 0;
+	int windchoice3 = 0;
+
+	int earthchoice1 = 0;
+	int earthchoice2 = 0;
+	int earthchoice3 = 0;
+
+	int waterchoice1 = 0;
+	int earthchoice2 = 0;
+	int earthchoice3 = 0;
+};
