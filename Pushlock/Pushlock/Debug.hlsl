c00128e0756ab52600c4b69ce0c5d4b4ca3758f9
Ipren <armas.94@hotmail.com>
2017-04-18 16:34:14 +0200

    added Lighting without a specular component

--- Pushlock/Pushlock/Debug.hlsl
+++ Pushlock/Pushlock/Debug.hlsl
@@ -8,6 +8,19 @@ cbuffer Mat : register(b1) {
 	float4 Color;
 }
 
+cbuffer dirLigth : register(b2)
+{
+    float3 dLightDirection;
+    float4 dLightcolor;
+}
+
+cbuffer camPos : register(b3)
+{
+    float4 cameraPosition;
+}
+
+static const float3 normal = float3(0.0f, 1.0f, 0.0f);
+
 float4 VS(float3 pos : POSITION) : SV_POSITION
 {
 	return mul(Proj, mul(View, mul(World, float4(pos, 1.0))));
@@ -15,5 +28,12 @@ float4 VS(float3 pos : POSITION) : SV_POSITION
 
 float4 PS(float4 pos : SV_POSITION) : SV_TARGET
 {
-	return Color;
+    float4 c = Color;
+    
+    float3 diffuse = saturate(dot(-dLightDirection, normal));
+    diffuse *= c.xyz * dLightcolor.xyz;
+
+    float3 ambient = c.xyz * float3(0.0f, 0.0f, 0.0f);
+
+    return float4(diffuse + ambient, 1.0f);
 }
