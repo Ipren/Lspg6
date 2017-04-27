bd4e7408170fc532dab5c46fe9643521cc10ff09
Ipren <armas.94@hotmail.com>
2017-04-27 15:27:16 +0200

    finnished the shaders

--- Pushlock/Pushlock/CUPS.hlsl
+++ Pushlock/Pushlock/CUPS.hlsl
@@ -1,4 +1,35 @@
-float4 main() : SV_TARGET
+struct VS_OUT
 {
-	return float4(1.0f, 1.0f, 1.0f, 1.0f);
+    float4 pos : SV_Position;
+    float2 uv : TEXCOORD;
+    int index : INDEX;
+};
+SamplerState sSampler;
+texture2D menu : register(t0);
+texture2D p1 : register(t1);
+texture2D p2 : register(t2);
+texture2D p3 : register(t3);
+texture2D p4 : register(t4);
+
+
+
+float4 main(VS_OUT input) : SV_TARGET
+{
+    if(input.index == 0)
+    {
+        return p1.Sample(sSampler, input.uv);
+    }
+    if (input.index == 1)
+    {
+        return p2.Sample(sSampler, input.uv);
+    }
+    if (input.index == 2)
+    {
+        return p3.Sample(sSampler, input.uv);
+    }
+    if (input.index == 3)
+    {
+        return p4.Sample(sSampler, input.uv);
+    }
+    return menu.Sample(sSampler, input.uv);
 }
--- Pushlock/Pushlock/CUVS.hlsl
+++ Pushlock/Pushlock/CUVS.hlsl
@@ -1,4 +1,26 @@
-float4 main( float4 pos : POSITION ) : SV_POSITION
+struct VS_IN
 {
-	return pos;
+    float4 pos : POSITION;
+    float2 uv : TEXCOORD;
+    int index : INDEX;
+};
+
+struct VS_OUT
+{
+    float4 pos : SV_Position;
+    float2 uv : TEXCOORD;
+    int index : INDEX;
+};
+
+
+
+VS_OUT main(VS_IN input)
+{
+    VS_OUT output;
+
+    output.pos = input.pos;
+    output.uv = input.uv;
+    output.index = input.index;
+
+    return output;
 }
