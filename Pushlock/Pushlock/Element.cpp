4c2e76ad16638b2048f4e05ede718468330aba8d
Ipren <armas.94@hotmail.com>
2017-04-27 11:53:36 +0200

    added stomp reset as a fire upgrade

--- Pushlock/Pushlock/Element.cpp
+++ Pushlock/Pushlock/Element.cpp
@@ -182,6 +182,11 @@ void FireElement::stomp(Player * player, Map * map)
 		for (auto result : nearby) { //moves all nearby players
 			result.entity->velocity.x += cos(result.angle) * (gSpellConstants.kFireStompStrength + gPlayerSpellConstants[player->index].kFireStompStrength) * abs((gSpellConstants.kFireStompDistance + gPlayerSpellConstants[player->index].kFireStompDistance + gSpellConstants.kFireStompStrengthFalloff + gPlayerSpellConstants[player->index].kFireStompStrengthFalloff) - result.distance);
 			result.entity->velocity.y += sin(result.angle) * (gSpellConstants.kFireStompStrength + gPlayerSpellConstants[player->index].kFireStompStrength) * abs((gSpellConstants.kFireStompDistance + gPlayerSpellConstants[player->index].kFireStompDistance + gSpellConstants.kFireStompStrengthFalloff + gPlayerSpellConstants[player->index].kFireStompStrengthFalloff) - result.distance);
+			
+			if (pUpgrades[player->index].choice[0] = 2)
+			{
+				player->element->cooldown[1] == 0.0f;
+			}
 		}
 
 		cooldown[2] = gSpellConstants.kFireStompCooldown + gPlayerSpellConstants[player->index].kFireStompCooldown;
