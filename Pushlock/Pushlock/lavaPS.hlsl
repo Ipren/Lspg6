a9a45bfc5c48b829721b9e931d38f44237de75d2
Ipren <armas.94@hotmail.com>
2017-05-16 12:20:19 +0200

    wrote lavaPS

--- Pushlock/Pushlock/lavaPS.hlsl
+++ Pushlock/Pushlock/lavaPS.hlsl
@@ -1,4 +1,61 @@
-float4 main() : SV_TARGET
+#include "Common.hlsl"
+
+texture2D mapTexture : register(t2);
+
+cbuffer Mat : register(b1)
 {
-	return float4(1.0f, 1.0f, 1.0f, 1.0f);
+    float4 Color;
+}
+
+cbuffer dirLigth : register(b2)
+{
+    float3 dLightDirection;
+    float4 dLightcolor;
+}
+
+cbuffer camPos : register(b3)
+{
+    float4 cameraPosition;
+}
+
+cbuffer pLightCount : register(b4)
+{
+    uint nrOfPointLights;
+}
+
+cbuffer heatHaze : register(b4)
+{
+    float offset;
+};
+
+StructuredBuffer<PointLight> pLights : register(t0);
+
+static const float3 normal = float3(0.0f, 1.0f, 0.0f);
+
+struct VS_OUT
+{
+    float4 pos : SV_Position;
+    float4 wPos : POSITION;
+    float4 viewPos : POSITION1;
+    float2 uv : TEXCOORD;
+};
+
+float4 main(in VS_OUT input) : SV_TARGET
+{
+
+    float2 texcoord = input.uv;
+    float4 c = mapTexture.Sample(ShadowSampler, texcoord);
+
+    
+    float3 lightDir = normalize(dLightDirection);
+    float3 diffuse = saturate(dot(normal, lightDir));
+    diffuse *= c.xyz * dLightcolor.xyz;
+
+    float4 coords = mul(ShadowProj, mul(ShadowView, mul(ShadowWorld, input.wPos)));
+    float shadow = GetShadow(normal, dLightDirection, coords);
+    float3 ambient = c.xyz * float3(0.0f, 0.0f, 0.0f);
+
+    diffuse += CalcPointLights(pLights, input.wPos, normal, nrOfPointLights);
+
+    return float4(diffuse + ambient + 0.2 * shadow, c.w);
 }
